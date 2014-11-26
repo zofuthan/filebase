2d5d3fa65a3b916487ad38936214ce469f1c0e9f
omeid <public@omeid.me>
2014-11-25 20:59:02 -0500

    Refactor ini for better standalone use.

--- codec/ini.go
+++ codec/ini.go
@@ -3,29 +3,15 @@ package codec
 import (
 	"io"
 
-	"github.com/omeid/filebase/codec/ini"
+	"github.com/omeid/go-ini"
 )
 
 type INI struct{}
 
 func (i INI) NewDecoder(r io.Reader) Decoder {
-	return ini_codec{r: r}
+	return ini.NewDecoder(r)
 }
 
 func (i INI) NewEncoder(w io.Writer) Encoder {
-	return ini_codec{w: w}
-}
-
-type ini_codec struct {
-	r io.Reader
-	w io.Writer
-}
-
-func (i ini_codec) Decode(v interface{}) error {
-
-	return ini.Decoder(i.r, v)
-}
-
-func (i ini_codec) Encode(v interface{}) error {
-	return ini.Encoder(v, i.w)
+	return ini.NewEncoder(w)
 }
